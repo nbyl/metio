e56e6801dddb3483ddc6d1f1deba887116cba907
Nico Byl <nico@nicolas-byl.eu>
2025-06-14 20:24:35 +0200

    full tofu instance

---
+++ .terraform.lock.hcl
@@ -0,0 +1,20 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/opentffoundation/google" {
+  version     = "6.39.0"
+  constraints = "6.39.0"
+  hashes = [
+    "h1:3L8/JYZol5UEhK53PL2196T2LdRJV9STLdAslEbOb3g=",
+    "zh:07cb55d572aaa5d929af125c3f43fdba68d9363b186d1c1d22871c8800f78e30",
+    "zh:1602787d200776e6159739fa4a0c0d4f4e95a1f860bd0a42cb95453720891b11",
+    "zh:30e244158a76d5de3a762505724796ad9d0b492c882f48a475ad29f4959e3b58",
+    "zh:47a59a9cad20e1f213686c4115d33370b500a493596199f8ed774df5c8f05707",
+    "zh:64ac49dce3702aa015edeeda436916152a738b7eabaf2721b8c982c60612f5a9",
+    "zh:729a1110f2aefe3e05915fb156f94ae5f7b316605a2c3ccffdcd499e070d9160",
+    "zh:754efb3057356b622dedaacd93305f370429aa3939763d65d4894f482bebee5f",
+    "zh:b316aa97e0be188f70ae82c4dea6b2a4fdb8bf265954ff8e0e47a54c786dc153",
+    "zh:b77191253616ba847c955d855734977e5512d64b045f945134fa407ba99a69b6",
+    "zh:dfb96666d9f5d69e62b46df5b93e31898d62011e818f57ea19342202110dd8c6",
+  ]
+}
--- main.tf
+++ main.tf
@@ -17,6 +17,22 @@ provider "google" {
   zone    = "europe-west3-a"
 }
 
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
+resource "google_service_account" "minecraft-server-sa" {
+  account_id   = "minecraft-server"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_disk" "primary" {
+  name  = "minecraft-data"
+  type  = "pd-ssd"
+
+  physical_block_size_bytes = 4096
+}
+
 resource "google_compute_instance" "minecraft-server" {
   name         = "minecraft-server"
   machine_type = "e2-micro"
@@ -27,9 +43,23 @@ resource "google_compute_instance" "minecraft-server" {
     }
   }
 
+  attached_disk {
+    source = google_compute_disk.primary.id
+    mode   = "READ_WRITE"
+  }
+
   network_interface {
     network = "default"
     access_config {
+      nat_ip = google_compute_address.static.address
     }
+  }
+
+  tags = ["minecraft-server"]
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.minecraft-server-sa.email
+    scopes = ["cloud-platform"]
   }
 }
