d6901d0de31697095cf0750d9a6681089dd5f9d2
Nicolas Byl <nico@nicolas-byl.eu>
2025-06-25 22:06:43 +0000

    instance can now be stopped

--- functions/index.ts
+++ functions/index.ts
@@ -36,3 +36,32 @@ export const startServer = functions.http('startServer', async (req, res) => {
 
   res.send(`Der Minecraft Server ist gestartet! Er ist jetzt erreichbar unter: ${statusResponse?.[0]?.networkInterfaces?.[0]?.accessConfigs?.[0]?.natIP || 'unbekannt'}:25565`);
 });
+
+export const stopServer = functions.http('stopServer', async (req, res) => {
+  const client = new compute.InstancesClient();
+
+  const response = await client.stop({
+    project: PROJECT,
+    zone: ZONE,
+    instance: 'minecraft-server'
+  });
+
+  console.log(`Response from stopping instance: ${JSON.stringify(response)}`);
+
+  var statusResponse = await client.get({
+    project: PROJECT,
+    zone: ZONE,
+    instance: 'minecraft-server'
+  });
+  while(statusResponse?.[0]?.status !== 'TERMINATED') {
+    console.log(`Instance status: ${statusResponse?.[0]?.status}`);
+    await delay(5000); // Wait for 5 seconds before checking again
+    statusResponse = await client.get({
+      project: PROJECT,
+      zone: ZONE,
+      instance: 'minecraft-server'
+    });
+  }
+
+  res.send(`Der Minecraft Server ist jetzt gestoppt! Der Status ist: ${statusResponse?.[0]?.status}`);
+});
