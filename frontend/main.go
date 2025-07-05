a1d5d404c9f71049df426d574e3d83bb3047dbe5
Nicolas Byl <nico@nicolas-byl.eu>
2025-07-05 07:49:07 +0000

    import of generated code from v0

---
+++ frontend/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type ServerStatus struct {
+	IsOnline   bool   `json:"isOnline"`
+	Players    int    `json:"players"`
+	MaxPlayers int    `json:"maxPlayers"`
+	Uptime     string `json:"uptime"`
+	Version    string `json:"version"`
+	IP         string `json:"ip"`
+	StartTime  time.Time `json:"-"`
+}
+
+type AccessLink struct {
+	URL    string    `json:"url"`
+	Expiry time.Time `json:"expiry"`
+}
+
+var (
+	serverStatus = ServerStatus{
+		IsOnline:   false,
+		Players:    0,
+		MaxPlayers: 20,
+		Uptime:     "00:00:00",
+		Version:    "1.20.4",
+		IP:         "mc.metio.server:25565",
+	}
+	currentAccessLink *AccessLink
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// Static files
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	// Routes
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/server/start", startServerHandler).Methods("POST")
+	r.HandleFunc("/server/stop", stopServerHandler).Methods("POST")
+	r.HandleFunc("/server/status", statusHandler).Methods("GET")
+	r.HandleFunc("/access/generate", generateAccessHandler).Methods("POST")
+
+	// Start uptime ticker
+	go uptimeTicker()
+
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func uptimeTicker() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		if serverStatus.IsOnline {
+			duration := time.Since(serverStatus.StartTime)
+			hours := int(duration.Hours())
+			minutes := int(duration.Minutes()) % 60
+			seconds := int(duration.Seconds()) % 60
+			serverStatus.Uptime = fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+		}
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	component := HomePage(serverStatus, currentAccessLink)
+	component.Render(r.Context(), w)
+}
+
+func startServerHandler(w http.ResponseWriter, r *http.Request) {
+	// Simulate startup delay
+	time.Sleep(3 * time.Second)
+
+	serverStatus.IsOnline = true
+	serverStatus.StartTime = time.Now()
+	serverStatus.Players = rand.Intn(5)
+	serverStatus.Uptime = "00:00:00"
+
+	component := ServerStatusCard(serverStatus)
+	component.Render(r.Context(), w)
+}
+
+func stopServerHandler(w http.ResponseWriter, r *http.Request) {
+	// Simulate shutdown delay
+	time.Sleep(2 * time.Second)
+
+	serverStatus.IsOnline = false
+	serverStatus.Players = 0
+	serverStatus.Uptime = "00:00:00"
+
+	component := ServerStatusCard(serverStatus)
+	component.Render(r.Context(), w)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	component := ServerStatusCard(serverStatus)
+	component.Render(r.Context(), w)
+}
+
+func generateAccessHandler(w http.ResponseWriter, r *http.Request) {
+	linkId := fmt.Sprintf("%d", rand.Int63())
+	expiry := time.Now().Add(24 * time.Hour)
+
+	currentAccessLink = &AccessLink{
+		URL:    fmt.Sprintf("https://metio.app/join/%s", linkId),
+		Expiry: expiry,
+	}
+
+	component := AccessLinkSection(currentAccessLink)
+	component.Render(r.Context(), w)
+}
