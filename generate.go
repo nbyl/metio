63a4accef9b433e0f04cc6464478920a6f14cc62
Nicolas Byl <nico@nicolas-byl.eu>
2025-07-10 08:26:29 +0000

    working on styles

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package frontend
 
 //go:generate go tool templ generate
 
-//go:generate npx tailwindcss -c ./tailwind.config.js -i ./static/globals.css -o ./static/styles.css --minify
+//go:generate npx tailwindcss -c ./tailwind.config.js -i ./static/globals.css -o ./static/styles.css
