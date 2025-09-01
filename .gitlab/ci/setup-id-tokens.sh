6226012d9e86bde5f4384cd36968833de03b0a47
Nicolas Byl <nico@nicolas-byl.eu>
2025-09-01 12:28:49 +0200

    login

--- .gitlab/ci/setup-id-tokens.sh
+++ .gitlab/ci/setup-id-tokens.sh
@@ -18,5 +18,7 @@ gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_P
       --service-account="${GCP_SERVICE_ACCOUNT}" \
       --output-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json" \
       --credential-source-file="${CI_PROJECT_DIR}/.ci_job_jwt_file"
+gcloud auth login --cred-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json"
+gcloud auth list
 
 export GOOGLE_APPLICATION_CREDENTIALS=${CI_PROJECT_DIR}/.gcp_temp_cred.json
