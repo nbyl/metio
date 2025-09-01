df3c948181f0b815a54cf8a1248e39283a508cc7
Nicolas Byl <nico@nicolas-byl.eu>
2025-09-01 09:47:45 +0000

    add full version

--- .gitlab/ci/setup-id-tokens.sh
+++ .gitlab/ci/setup-id-tokens.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-env
+apk add --update --no-cache curl ca-certificates bash python3
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar xzf google-cloud-cli-linux-x86_64.tar.gz
+/google-cloud-sdk/install.sh
+
+export PATH="/google-cloud-sdk/bin:${PATH}"
+
+gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_PROVIDER}" \
+      --service-account="${GCP_SERVICE_ACCOUNT}" \
+      --output-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json" \
+      --credential-source-file="${CI_PROJECT_DIR}/.ci_job_jwt_file"
+
+export GOOGLE_APPLICATION_CREDENTIALS=${CI_PROJECT_DIR}/.gcp_temp_cred.json
