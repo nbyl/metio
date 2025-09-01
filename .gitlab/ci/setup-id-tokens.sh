c9d80771da44a0f9e7563e91360d7618dcf67b3c
Nicolas Byl <nico@nicolas-byl.eu>
2025-09-01 11:54:10 +0200

    fix jwt file

--- .gitlab/ci/setup-id-tokens.sh
+++ .gitlab/ci/setup-id-tokens.sh
@@ -10,6 +10,7 @@ cd "${CI_PROJECT_DIR}" || exit 1
 
 export PATH="/google-cloud-sdk/bin:${PATH}"
 
+echo "${GITLAB_TOKEN}" > "${CI_PROJECT_DIR}/.ci_job_jwt_file"
 gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_PROVIDER}" \
       --service-account="${GCP_SERVICE_ACCOUNT}" \
       --output-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json" \
