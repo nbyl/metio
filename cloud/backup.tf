66768676de15f9915978558a8c038485f9d51ed9
Nicolas Byl <nico@nicolas-byl.eu>
2025-09-08 09:05:30 +0200

    add backup container to new VM

--- cloud/backup.tf
+++ cloud/backup.tf
@@ -1,10 +1,6 @@
 resource "google_storage_bucket" "minecraft-backups" {
-  name          = "${terraform.workspace}-minecraft-backups-bucket"
-  location      = "europe-west3"
-  force_destroy = true
-
+  name                     = "${terraform.workspace}-minecraft-backups-bucket"
+  location                 = "europe-west3"
+  force_destroy            = true
   public_access_prevention = "enforced"
-  retention_policy {
-    retention_period = 7776000 # 90 days in seconds
-  }
 }
