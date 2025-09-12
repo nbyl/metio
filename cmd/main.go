900b02e3a6f15c300f3045b4b23f36a2f847bb33
Nicolas Byl <nico@nicolas-byl.eu>
2025-09-12 10:26:27 +0000

    add viper for configuration

--- cmd/main.go
+++ cmd/main.go
@@ -7,21 +7,17 @@ import (
 	"os"
 
 	gorillahandlers "github.com/gorilla/handlers"
+	"github.com/spf13/viper"
 	"gitlab.com/nbyl/metio/handlers"
 )
 
-func getEnv(key, fallback string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		value = fallback
-	}
-	return value
-}
-
 func main() {
+	viper.AutomaticEnv()
+	viper.SetDefault("PORT", "8080")
+
 	r := handlers.New()
 
-	port := getEnv("PORT", "8080")
+	port := viper.GetString("PORT")
 	log.Printf("Server starting on :%s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), gorillahandlers.LoggingHandler(os.Stdout, r)))
 }
