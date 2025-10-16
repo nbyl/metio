f519fb81e4543f87f2e8fbb0febc32a1100e4295
Nico Byl <nico@nicolas-byl.eu>
2025-10-16 15:50:57 +0000

    count number of minecraft players

--- cmd/machine-agent/main.go
+++ cmd/machine-agent/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("hello world")
+	viper.SetDefault("MINECRAFT_CHECK_INTERVAL", "30s")
+	viper.AutomaticEnv()
+
+	intervalStr := viper.GetString("MINECRAFT_CHECK_INTERVAL")
+	interval, err := time.ParseDuration(intervalStr)
+	if err != nil {
+		log.Fatalf("Invalid interval format: %v", err)
+	}
+
+	fmt.Printf("Machine agent started with check interval: %v\n", interval)
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	go func() {
+		for range ticker.C {
+			current, max, err := getMinecraftPlayerCount()
+			if err != nil {
+				log.Printf("Error getting player count: %v", err)
+			} else {
+				log.Printf("Minecraft players online: %d/%d", current, max)
+			}
+		}
+	}()
+
+	// Keep the program running
+	select {}
+}
+
+func getMinecraftPlayerCount() (int, int, error) {
+	cmd := exec.Command("docker", "exec", "minecraft", "rcon-cli", "--password", "minecraft2025", "list")
+	output, err := cmd.Output()
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Parse output like "There are 2 of a max of 20 players online: Steve, Alex"
+	re := regexp.MustCompile(`There are (\d+) of a max of (\d+) players online`)
+	matches := re.FindStringSubmatch(string(output))
+	if len(matches) < 3 {
+		return 0, 0, fmt.Errorf("could not parse player count from output: %s", string(output))
+	}
+
+	current, _ := strconv.Atoi(matches[1])
+	max, _ := strconv.Atoi(matches[2])
+	return current, max, nil
 }
