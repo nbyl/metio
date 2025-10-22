183f80046cbdc77502bceeb62e9b3eed66b79773
Nicolas Byl <nicolas.byl@eon.com>
2025-10-22 22:29:38 +0000

    try other login

--- .gitlab/ci/setup-application-credentials.sh
+++ .gitlab/ci/setup-application-credentials.sh
@@ -10,5 +10,3 @@ gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_P
       --credential-source-file="${CI_PROJECT_DIR}/.ci_job_jwt_file"
 gcloud auth login --cred-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json"
 gcloud auth list
-
-export GOOGLE_APPLICATION_CREDENTIALS=${CI_PROJECT_DIR}/.gcp_temp_cred.json
