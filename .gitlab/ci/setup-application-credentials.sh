3337dbbf464fdc20ca504a5829ffc7d4fc5c7d7e
Nicolas Byl <nicolas.byl@eon.com>
2025-10-22 22:09:59 +0000

    fix login

---
+++ .gitlab/ci/setup-application-credentials.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo "${GITLAB_OIDC_TOKEN}" > "${CI_PROJECT_DIR}/.ci_job_jwt_file"
+gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_PROVIDER}" \
+      --service-account="${GCP_SERVICE_ACCOUNT}" \
+      --output-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json" \
+      --credential-source-file="${CI_PROJECT_DIR}/.ci_job_jwt_file"
+gcloud auth login --cred-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json"
+gcloud auth list
+
+export GOOGLE_APPLICATION_CREDENTIALS=${CI_PROJECT_DIR}/.gcp_temp_cred.json
