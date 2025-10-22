21c31e87b7aa1baf3f8e1f8418ad90747ba4459f
Nicolas Byl <nicolas.byl@eon.com>
2025-10-22 22:33:25 +0000

    set project in script

--- .gitlab/ci/setup-application-credentials.sh
+++ .gitlab/ci/setup-application-credentials.sh
@@ -10,3 +10,4 @@ gcloud iam workload-identity-pools create-cred-config "${GCP_WORKLOAD_IDENTITY_P
       --credential-source-file="${CI_PROJECT_DIR}/.ci_job_jwt_file"
 gcloud auth login --cred-file="${CI_PROJECT_DIR}/.gcp_temp_cred.json"
 gcloud auth list
+gcloud config set project "${TF_VAR_project_id}"
