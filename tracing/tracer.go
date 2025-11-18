6b0eec9380162d60c6c650974065ee1f138457ba
Nicolas Byl <nicolas.byl@eon.com>
2025-11-18 11:54:49 +0000

    sending cloud traces to GCP

--- tracing/tracer.go
+++ tracing/tracer.go
@@ -7,10 +7,14 @@ import (
 
 	"go.opentelemetry.io/contrib/detectors/gcp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/oauth"
 )
 
 var tp *sdktrace.TracerProvider
@@ -26,11 +30,18 @@ func InitTracerWithDetails(serviceName, serviceVersion string) error {
 		log.Printf("Warning: GOOGLE_CLOUD_PROJECT not set, using default detection")
 	}
 
+	// Retrieve and store Google application-default credentials
+	creds, err := oauth.NewApplicationDefault(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
 	res, err := resource.New(context.Background(),
 		resource.WithDetectors(gcp.NewDetector()),
 		resource.WithAttributes(
 			semconv.ServiceName(serviceName),
 			semconv.ServiceVersion(serviceVersion),
+			attribute.String("gcp.project_id", projectID),
 		),
 	)
 	if err != nil {
@@ -42,9 +53,10 @@ func InitTracerWithDetails(serviceName, serviceVersion string) error {
 		"X-Goog-User-Project": projectID,
 	}
 
-	exporter, err := otlptracehttp.New(context.Background(),
-		otlptracehttp.WithEndpoint("https://cloudtrace.googleapis.com:443"),
-		otlptracehttp.WithHeaders(headers),
+	exporter, err := otlptracegrpc.New(context.Background(),
+		otlptracegrpc.WithEndpoint("telemetry.googleapis.com"),
+		otlptracegrpc.WithHeaders(headers),
+		otlptracegrpc.WithDialOption(grpc.WithPerRPCCredentials(creds)),
 	)
 	if err != nil {
 		log.Printf("Failed to create Cloud Trace exporter: %v", err)
