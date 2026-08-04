b7666dc39c01ee6c2457e02a23a6c96b99354ae7
Nicolas Byl <nico@nicolas-byl.eu>
2026-08-04 12:15:13 +0200

    feat: remove Firestore resources and datastore permissions (#328)

--- deploy/modules/gcp-cloud-run/main.tf
+++ deploy/modules/gcp-cloud-run/main.tf
@@ -13,10 +13,3 @@ resource "random_id" "default" {
 data "google_project" "current" {
   project_id = var.project_id
 }
-
-resource "google_firestore_database" "dapr_statestore" {
-  project     = var.project_id
-  name        = "(default)"
-  location_id = var.region
-  type        = "DATASTORE_MODE"
-}
